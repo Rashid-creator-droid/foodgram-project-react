5474bd3d3d9dc0080ad5bd01c09afc334d0095fd
Rashid Ayupov <valentaine1234@gmail.com>
2023-05-26 12:01:01 +0300

    Actions, Readme, settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -20,9 +20,9 @@ ALLOWED_HOSTS = ['158.160.99.26', 'backend', 'backend:8000']
 
 # CSRF
 
-CSRF_TRUSTED_ORIGINS = ['http://*.158.160.99.26']
+CSRF_TRUSTED_ORIGINS = ['http://*.158.160.99.26', 'http://158.160.99.26']
 CSRF_COOKIE_SECURE = False
-CSRF_COOKIE_DOMAIN = 'backend'
+CSRF_COOKIE_DOMAIN = '.158.160.99.26'
 
 # Application definition
 
